18d69514feebf48574dd08c26537091e702c5738
eleadn <eleano.dn@gmail.com>
2025-06-11 23:28:39 +0200

    remove contnet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 #!/usr/bin/env node
-
-const f = require("./fibonacci");
-
-const iter = 15;
-
-const fibsRes = f.fibs(iter).toString();
-const fibsRecRes = f.fibsRec(iter).toString();
-const isEqual = fibsRes === fibsRecRes;
-
-console.log(`Number of iterations: ${iter}`);
-console.log(`Non recursive fibonacci: ${fibsRes}`);
-console.log(`Recursive fibonacci: ${fibsRecRes}`);
-console.log(`f.fibs(${iter}) === f.fibsRec(${iter}) ? ${isEqual}`);
