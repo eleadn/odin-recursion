9217d5bdbd382c62ebbd022b3cde83937c79caba
eleadn <eleano.dn@gmail.com>
2025-06-11 23:28:00 +0200

    Improve logs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@
 
 const f = require("./fibonacci");
 
-console.log(f.fibsRec(8));
+const iter = 15;
+
+const fibsRes = f.fibs(iter).toString();
+const fibsRecRes = f.fibsRec(iter).toString();
+const isEqual = fibsRes === fibsRecRes;
+
+console.log(`Number of iterations: ${iter}`);
+console.log(`Non recursive fibonacci: ${fibsRes}`);
+console.log(`Recursive fibonacci: ${fibsRecRes}`);
+console.log(`f.fibs(${iter}) === f.fibsRec(${iter}) ? ${isEqual}`);
